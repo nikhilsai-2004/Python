a81af5aa798e646d0d56a022ca935bbe65461d45
Nikhil sai <nikhilsai@Nikhils-MacBook-Air.local>
2023-07-04 15:02:03 +0530

    file has been added

---
+++ main.py
@@ -0,0 +1,23 @@
+base = 3
+exponent = 4
+
+result = 1
+
+while exponent != 0:
+    result *= base
+    exponent-=1
+
+print("Answer = " + str(result))
+
+#write a function to calculate a^n 
+
+base = 10
+exponent = 2
+
+result = 1
+
+while exponent != 0:
+    result *= base
+    exponent-=1
+
+print("Answer = " + str(result))
